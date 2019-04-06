7c990c6abfa6c18c4fd87a27ba944fafd3d6b8bd
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-04-06 14:25:50 +0200

    POJO and User made

---
+++ src/main/java/pl/sda/mongo_APAW/mongo/MongoPojoExample.java
@@ -0,0 +1,35 @@
+package pl.sda.mongo_APAW.mongo;
+
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.model.Filters;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
+public class MongoPojoExample {
+
+    public static void main(String[] args) {
+        CodecRegistry codecRegistry = fromRegistries(
+                MongoClient.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().automatic(true).build()));
+        MongoClientSettings settings = MongoClientSettings.builder()
+                .codecRegistry(codecRegistry)
+                .build();
+        com.mongodb.client.MongoClient mongoClient = MongoClients.create(settings);
+
+        MongoCollection<User> collection = mongoClient.getDatabase("new").getCollection("uzytkownicy", User.class);
+
+        for (User user : collection.find()) {
+            System.out.println(user.getLastname());
+        }
+
+//        collection.insertOne(new User("abc", "cde", 134));
+
+//        collection.deleteOne(Filters.eq("name", "Andy1"));
+
+    }
+}
---
+++ src/main/java/pl/sda/mongo_APAW/mongo/User.java
@@ -0,0 +1,62 @@
+package pl.sda.mongo_APAW.mongo;
+
+import org.bson.types.ObjectId;
+
+public class User {
+
+    private ObjectId id;
+    private String name;
+    private String lastname;
+    private int age;
+
+    public User() {
+    }
+
+    public User(String name, String lastname, int age) {
+        this.name = name;
+        this.lastname = lastname;
+        this.age = age;
+    }
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", age=" + age +
+                '}';
+    }
+}
