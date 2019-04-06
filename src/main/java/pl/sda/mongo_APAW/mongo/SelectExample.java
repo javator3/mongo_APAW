45acf48a50ae00c2880bc79a63fa2879776a0e3d
Andrzej Pawlowski <panandy@poczta.onet.pl>
2019-04-06 13:14:13 +0200

    filters added

--- src/main/java/pl/sda/mongo_APAW/mongo/SelectExample.java
+++ src/main/java/pl/sda/mongo_APAW/mongo/SelectExample.java
@@ -5,6 +5,7 @@ import com.mongodb.client.FindIterable;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.model.Filters;
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class SelectExample {
 
@@ -14,6 +15,9 @@ public class SelectExample {
         MongoClient mongoClient = new MongoClient();
         MongoCollection<Document> collection = mongoClient.getDatabase("new").getCollection("uzytkownicy");
 
+//        showAge(5, collection);
+//        showById(collection);
+        showBeetwen2and4(2, collection);
 
 //        for (Document document : collection.find()) {
 //            System.out.println(document.toJson());
@@ -21,9 +25,47 @@ public class SelectExample {
 //        Document first = collection.find().first();
 //        System.out.println(first);
 
-        FindIterable<Document> iterable = collection.find(Filters.eq("name", "Adam"));
-        for(Document iterabl : iterable) {
-            System.out.println(iterabl);
+//        FindIterable<Document> iterable = collection.find(Filters.eq("name", "Adam"));
+//        for(Document iterabl : iterable) {
+//            System.out.println(iterabl);
+//        }
+
+//        Document first1 = collection.find(Filters.eq("lastname", "Kowalski")).first();
+//        System.out.println(first1);
+    }
+
+    //napisz metodę która wypisze na eranie wszystkie elementy z kolekcji o wieku podanym w argumencie metody
+
+    static void showAge(int n, MongoCollection<Document> collection) {
+
+        FindIterable<Document> age1 = collection.find(Filters.eq("age", n));
+        for (Document age : age1) {
+            System.out.println(age);
         }
     }
+
+    // napisz metode która wypisze na ekran elemnety z kolekcji o podanym id, w przypadku gdy taki element nie istnieje w tabeli to zwróć odpowiedni komunikat
+
+    static void showById(MongoCollection<Document> collection) {
+
+        Document first2 = collection.find(Filters.eq("_id", new ObjectId("5ca876cb50d1b503d38fe897"))).first();
+        if (first2 == null) {
+            System.out.println("brak uzytkownika");
+        } else {
+
+            System.out.println(first2);
+        }
+    }
+
+    // napisz metodę która wypisze elementy z kolekcji od 2 do 4 gdzie wiek jest >= niż podany przez użytkownika
+
+    static void showBeetwen2and4(int n, MongoCollection<Document> collection) {
+
+        FindIterable<Document> age2 = collection.find(Filters.gte("age", n)).skip(1).limit(3);
+        for (Document age3: age2) {
+            System.out.println(age3);
+        }
+    }
+
+
 }
